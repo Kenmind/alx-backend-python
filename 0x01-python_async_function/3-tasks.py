ff32ba6e98a67837d124597426fc1530681b36a9
Kenmind <kennedykalaluka04@gmail.com>
2022-06-27 12:57:31 +0200

    :wrench: Type Annotation

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -4,6 +4,6 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> _asyncio.Task:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """Creates an asynchronous task for wait_random"""
     return asyncio.create_task(wait_random(max_delay))
