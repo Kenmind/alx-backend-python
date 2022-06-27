db0de1ab8f14d900718a4ceeee4bde98d565a975
Kenmind <kennedykalaluka04@gmail.com>
2022-06-27 12:54:40 +0200

    Type Annotation

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -5,7 +5,7 @@ import time
 wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
-def measure_time(n, max_delay):
+def measure_time(n: int, max_delay: int) -> float:
     """Calculates the runtime of wait_n"""
     start = time.time()
     asyncio.run(wait_n(n, max_delay))
